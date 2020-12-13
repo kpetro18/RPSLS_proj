e67cd74dc9cf79be63598eeacca9e774bb9b81ab
kevin petroviak <kpetro18@gmail.com>
2020-12-13 00:12:15 -0600

    reorganized rules listing to be followed easier

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -8,7 +8,27 @@ namespace RPSLS
 {
     class Game
     {
+        //member variables (has a)
 
+        //constructor (spawner)
+
+        //member methods (can do)
+        public void Rules()
+        {
+            Console.WriteLine("Rock Paper Scissors Lizard Spock");
+            Console.WriteLine("Rules:");
+            Console.WriteLine("Scissors cuts Paper");
+            Console.WriteLine("Scissors decapitates Lizard");
+            Console.WriteLine("Paper covers Rock");
+            Console.WriteLine("Paper disproves Spock");
+            Console.WriteLine("Lizard poisons Spock");
+            Console.WriteLine("Lizard eats Paper");
+            Console.WriteLine("Spock smashes Scissors");
+            Console.WriteLine("Spock vaporizes Rock");
+            Console.WriteLine("Rock crushes Lizard");
+            Console.WriteLine("And as it always has Rock crushes Scissors");
+            Console.WriteLine("Enter '1' For single player, '2' For two player");
+        }
 
 
 
